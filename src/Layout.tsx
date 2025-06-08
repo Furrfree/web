f779864999e6bd99e76bef9d3b41d8f9faac784b
tekofx <tekofxx@gmail.com>
2025-06-08 12:41:50 +0200

    Added animations when changing page

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,31 +1,24 @@
 import {AppShell} from "@mantine/core";
-import {Outlet} from "react-router";
 import NavBar from "./components/NavBar.tsx";
 import {useMediaQuery} from "@mantine/hooks";
-
+import AnimatedOutlet from "./main.tsx";
 
 export default function Layout() {
-
     const isMobile = useMediaQuery('(max-width: 48em)');
-
+    //const location = useLocation();
 
     return (
-        <AppShell
-            header={{height: 60}}
-            padding="md"
-            w={"100vw"}
-        >
+        <AppShell header={{height: 60}} padding="md" w={"100vw"}>
             <AppShell.Header>
                 <NavBar/>
             </AppShell.Header>
-
             <AppShell.Main
                 style={{
                     paddingLeft: isMobile ? 50 : 150,
                     paddingRight: isMobile ? 50 : 150,
                 }}
             >
-                <Outlet/>
+                <AnimatedOutlet/>
             </AppShell.Main>
         </AppShell>
     );
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import {createRoot} from 'react-dom/client'
 import '@mantine/core/styles.css';
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router";
+import {BrowserRouter, Route, Routes, useLocation, useOutlet} from "react-router";
 import theme from "./theme/theme.tsx";
 import {MantineProvider} from "@mantine/core";
 import Index from "./pages";
@@ -10,14 +10,14 @@ import Meetings from "./pages/meetings.tsx";
 import About from "./pages/about.tsx";
 import Errors from "./pages/Errors.tsx";
 import Layout from "./Layout.tsx";
+import {AnimatePresence, motion} from "framer-motion";
 
 
-
-const router = createBrowserRouter([
+/*const router = createBrowserRouter([
     {
         path: import.meta.env.BASE_URL,
         errorElement: <Errors/>, // Root-level error element
-        element: <Layout/>,
+        Component: Layout,
         children: [
             {path: "", Component: Index},
             {path: `${import.meta.env.BASE_URL}groups`, Component: Groups},
@@ -25,11 +25,40 @@ const router = createBrowserRouter([
             {path: `${import.meta.env.BASE_URL}about`, Component: About},
         ]
     }
-]);
-
+]);*/
+const AnimatedOutlet = () => {
+    const outlet = useOutlet();
+    const location = useLocation();
+    return (
+        <AnimatePresence mode="wait">
+            {outlet && (
+                <motion.div
+                    key={location.pathname}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
+                >
+                    {outlet}
+                </motion.div>
+            )}
+        </AnimatePresence>
+    );
+};
 
 createRoot(document.getElementById('root')!).render(
     <MantineProvider theme={theme} defaultColorScheme="dark">
-        <RouterProvider router={router}/>
+
+        <BrowserRouter>
+            <Routes>
+                <Route path={import.meta.env.BASE_URL} element={<Layout/>}>
+                    <Route index element={<Index/>}/>
+                    <Route path="groups" element={<Groups/>}/>
+                    <Route path="meetings" element={<Meetings/>}/>
+                    <Route path="about" element={<About/>}/>
+                    <Route path="*" element={<Errors/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </MantineProvider>
 )
+export default AnimatedOutlet

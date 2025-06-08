0708ab8bdf77678cdd3fbd3878845aa7503ac5ed
tekofx <tekofxx@gmail.com>
2025-06-08 12:44:09 +0200

    Updated links

--- src/data/groups/groups.tsx
+++ src/data/groups/groups.tsx
@@ -1,9 +1,9 @@
 import {IconBrandDiscord, IconBrandTelegram} from "@tabler/icons-react";
 
 const groups = [
-    {"name": "Furrfree", "icon": IconBrandTelegram, link: "https://t.me/furrfree"},
+    {"name": "Furrfree España", "icon": IconBrandTelegram, link: "https://t.me/FurrFreeES"},
     {"name": "Furrfree Internacional", "icon": IconBrandTelegram, link: "https://t.me/furrfree"},
-    {"name": "Furrfree", "icon": IconBrandDiscord, link: "https://t.me/furrfree"},
+    {"name": "Furrfree", "icon": IconBrandDiscord, link: "https://discord.gg/fZb2PFtK8B"},
 ]
 
 export default groups;

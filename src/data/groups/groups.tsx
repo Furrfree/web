17ff8e5c2e52166ececd74f9ddebe534e6831355
tekofx <tekofxx@gmail.com>
2025-06-08 10:59:59 +0200

    Moved groups to data

---
+++ src/data/groups/groups.tsx
@@ -0,0 +1,9 @@
+import {IconBrandDiscord, IconBrandTelegram} from "@tabler/icons-react";
+
+const groups = [
+    {"name": "Furrfree", "icon": IconBrandTelegram, link: "https://t.me/furrfree"},
+    {"name": "Furrfree Internacional", "icon": IconBrandTelegram, link: "https://t.me/furrfree"},
+    {"name": "Furrfree", "icon": IconBrandDiscord, link: "https://t.me/furrfree"},
+]
+
+export default groups;
--- src/pages/groups.tsx
+++ src/pages/groups.tsx
@@ -1,3 +1,32 @@
+import {SimpleGrid, Stack, Text} from "@mantine/core";
+import GroupCard from "../components/GroupCard.tsx";
+import Title2 from "../components/texts/Title2.tsx";
+import groups from "../data/groups/groups.tsx";
+
+
 export default function Groups() {
-    return <h1>Welcome to the Groups Page</h1>;
+    return (
+        <Stack>
+            <Title2 text="Grupos" align="center"/>
+            <Text>
+                En furrfree hay diferentes grupos a los que puedes unirte para poder formar parte de esta
+                comunidad.
+            </Text>
+            <SimpleGrid
+                cols={{base: 1, xs: 1, sm: 1, md: 2, lg: 3}}
+                spacing={{base: 10, sm: 'xl'}}
+                verticalSpacing={{base: 'md', sm: 'xl'}}
+            >
+                {groups.map((group, index) => (
+                    <GroupCard
+                        key={index}
+                        name={group.name}
+                        icon={group.icon}
+                        imageUrl={`${import.meta.env.BASE_URL}vite.svg`}
+                        link={group.link}
+                    />
+                ))}
+            </SimpleGrid>
+        </Stack>
+    )
 }

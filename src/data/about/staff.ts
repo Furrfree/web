2af4025cbdad15373b911b4327c0e08bfce90ee8
tekofx <tekofxx@gmail.com>
2025-06-09 16:16:18 +0200

    Added volpinho to staff

--- src/data/about/staff.ts
+++ src/data/about/staff.ts
@@ -1,34 +1,39 @@
-const staff=[
-  {
-    "name": "Etzharai",
-    "position": "Fundador/a",
-    "image": `${import.meta.env.BASE_URL}staff/etzharai.jpg`
-  },
-  {
-    "name": "Teko",
-    "position": "Programador",
-    "image": `${import.meta.env.BASE_URL}staff/teko.jpg`
-  },
-  {
-    "name": "Orenji",
-    "position": "Moderador",
-    "image": `${import.meta.env.BASE_URL}staff/orenji.jpg`
-  },
-  {
-    "name": "Staga",
-    "position": "Moderadora",
-    "image": `${import.meta.env.BASE_URL}staff/staga.jpg`
-  },
-  {
-    "name": "Rookie",
-    "position": "Moderador",
-    "image": `${import.meta.env.BASE_URL}staff/rookie.jpg`
-  },
-  {
-    "name": "Tori",
-    "position": "Moderador",
-    "image": `${import.meta.env.BASE_URL}staff/tori.jpg`
-  }
+const staff = [
+    {
+        "name": "Etzharai",
+        "position": "Fundador/a",
+        "image": `${import.meta.env.BASE_URL}staff/etzharai.jpg`
+    },
+    {
+        "name": "Teko",
+        "position": "Programador",
+        "image": `${import.meta.env.BASE_URL}staff/teko.jpg`
+    },
+    {
+        "name": "Orenji",
+        "position": "Moderador",
+        "image": `${import.meta.env.BASE_URL}staff/orenji.jpg`
+    },
+    {
+        "name": "Staga",
+        "position": "Moderadora",
+        "image": `${import.meta.env.BASE_URL}staff/staga.jpg`
+    },
+    {
+        "name": "Rookie",
+        "position": "Moderador",
+        "image": `${import.meta.env.BASE_URL}staff/rookie.jpg`
+    },
+    {
+        "name": "Tori",
+        "position": "Moderador",
+        "image": `${import.meta.env.BASE_URL}staff/tori.jpg`
+    },
+    {
+        "name": "Volpinho",
+        "position": "Diseñador",
+        "image": `${import.meta.env.BASE_URL}staff/volpinho.jpg`
+    }
 ]
 
 export default staff;

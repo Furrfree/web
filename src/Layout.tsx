13599d1678177d25b93936d39b412cbf4a13b5f9
tekofx <tekofxx@gmail.com>
2025-06-09 12:26:44 +0200

    Changed navbar on mobile

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,16 +1,49 @@
-import {AppShell} from "@mantine/core";
+import {AppShell, Button, Stack} from "@mantine/core";
 import NavBar from "./components/NavBar.tsx";
-import {useMediaQuery} from "@mantine/hooks";
+import {useDisclosure, useMediaQuery} from "@mantine/hooks";
 import AnimatedOutlet from "./main.tsx";
+import routes from "./utils/routes.ts";
+import {useNavigate} from "react-router";
 
 export default function Layout() {
     const isMobile = useMediaQuery('(max-width: 48em)');
+    const [opened, { toggle}] = useDisclosure(false);
+    const navigate = useNavigate()
+
 
     return (
-        <AppShell header={{height: 60}} padding="md" w={"100vw"}>
+        <AppShell
+            header={{height: 60}}
+            navbar={{
+                width: 300,
+                breakpoint: 'sm',
+                collapsed: {mobile: !opened , desktop: true},
+            }}
+            padding="md"
+            w={"100vw"}
+        >
             <AppShell.Header>
-                <NavBar/>
+                <NavBar onBurgerClick={toggle} opened={opened} />
             </AppShell.Header>
+            <AppShell.Navbar>
+                <Stack>
+                    {routes.map(link => (
+                        <Button
+                            variant="transparent"
+                            key={link.href}
+                            size="xl"
+                            style={{
+                                color: "white",
+                                textDecoration: location.pathname === link.href ? "underline" : "none",
+                            }}
+                            onClick={() => navigate(link.href)}
+                        >
+                            {link.label}
+                        </Button>
+                    ))}
+                </Stack>
+
+            </AppShell.Navbar>
             <AppShell.Main
                 style={{
                     paddingLeft: isMobile ? 50 : 150,
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,36 +1,48 @@
-import {Button, Group} from "@mantine/core";
+import {Button, Group, Burger} from "@mantine/core";
+import {useMediaQuery} from "@mantine/hooks";
 import logo from "../assets/logo.png";
 import {useLocation, useNavigate} from "react-router";
 import routes from "../utils/routes.ts";
 
-export default function NavBar() {
+
+interface NavBarProps {
+    onBurgerClick: () => void;
+    opened: boolean
+}
+
+export default function NavBar({onBurgerClick, opened}: NavBarProps) {
     const location = useLocation();
     const navigate = useNavigate();
-
+    const isMobile = useMediaQuery('(max-width: 768px)');
 
     return (
         <Group
-            gap="xl"
+            gap="xs"
             style={{
                 background: "linear-gradient(90deg, #7AA5FC 0%, #E072F0 100%)",
             }}
+            justify={isMobile ? "space-between" : "flex-start"}
         >
             <img src={logo} alt="Logo" style={{height: "60px"}}/>
-            <Group>
-                {routes.map(link => (
-                    <Button
-                        variant="transparent"
-                        key={link.href}
-                        style={{
-                            color: "black",
-                            textDecoration: location.pathname === link.href ? "underline" : "none",
-                        }}
-                        onClick={() => navigate(link.href)}
-                    >
-                        {link.label}
-                    </Button>
-                ))}
-            </Group>
+            {isMobile ? (
+                <Burger opened={opened} onClick={() => onBurgerClick()} style={{paddingRight: 50}}/>
+            ) : (
+                <Group>
+                    {routes.map(link => (
+                        <Button
+                            variant="transparent"
+                            key={link.href}
+                            style={{
+                                color: "black",
+                                textDecoration: location.pathname === link.href ? "underline" : "none",
+                            }}
+                            onClick={() => navigate(link.href)}
+                        >
+                            {link.label}
+                        </Button>
+                    ))}
+                </Group>
+            )}
         </Group>
     );
 }

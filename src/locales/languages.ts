d1c55f6669c92eb571b05a78e4084790c4773bb7
tekofx <tekofxx@gmail.com>
2025-06-09 18:14:06 +0200

    Commented unavailable languages

--- src/locales/languages.ts
+++ src/locales/languages.ts
@@ -1,9 +1,9 @@
 const languages = [
     {id: "es", name: "Español"},
     {id: "en", name: "English"},
-    {id: "eu", name: "Euskera"},
+    /*{id: "eu", name: "Euskera"},
     {id: "gl", name: "Galego"},
-    {id: "ca", name: "Català"},
+    {id: "ca", name: "Català"},*/
 ]
 
 export default languages;

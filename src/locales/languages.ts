5f3be13da6926dddcde25af514ab3bc6dd89dae3
tekofx <tekofxx@gmail.com>
2025-06-18 13:02:42 +0200

    Added catalan and auto language selection

--- src/locales/languages.ts
+++ src/locales/languages.ts
@@ -1,6 +1,7 @@
 const languages = [
     {id: "es", name: "Español"},
-    {id: "en", name: "English"},
+    {id: "en-US", name: "English"},
+    {id: "ca", name: "Català"}
     /*{id: "eu", name: "Euskera"},
     {id: "gl", name: "Galego"},
     {id: "ca", name: "Català"},*/

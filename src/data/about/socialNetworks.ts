b528953853b06327b0dd75cd3c195fd9bb894cd7
tekofx <tekofxx@gmail.com>
2025-07-19 19:01:44 +0200

    Replaced X logo with Twitter logo

--- src/data/about/socialNetworks.ts
+++ src/data/about/socialNetworks.ts
@@ -1,16 +1,16 @@
-import {IconBrandBluesky, IconBrandX} from "@tabler/icons-react";
+import { IconBrandBluesky, IconBrandTwitter } from "@tabler/icons-react";
 
 const socials = [
-    {
-        name: "Bluesky",
-        url: "https://bsky.app/profile/furrfree.bsky.social",
-        icon: IconBrandBluesky,
-    },
-    {
-        name: "X",
-        url: "https://x.com/furrfree",
-        icon: IconBrandX
-    },
-]
+  {
+    name: "Bluesky",
+    url: "https://bsky.app/profile/furrfree.bsky.social",
+    icon: IconBrandBluesky,
+  },
+  {
+    name: "X",
+    url: "https://x.com/furrfree",
+    icon: IconBrandTwitter,
+  },
+];
 
 export default socials;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,57 +1,58 @@
 import StaffCard from "../components/cards/StaffCard.tsx";
-import {ActionIcon, Group, SimpleGrid, Stack} from "@mantine/core";
+import { ActionIcon, Group, SimpleGrid, Stack } from "@mantine/core";
 import Title2 from "../components/texts/Title2.tsx";
 import socialNetworks from "../data/about/socialNetworks.ts";
 import staff from "../data/about/staff.ts";
-import {useTranslation} from "react-i18next";
-import {useMediaQuery} from "@mantine/hooks";
+import { useTranslation } from "react-i18next";
+import { useMediaQuery } from "@mantine/hooks";
 
 export default function About() {
-    const {t} = useTranslation(["pages", "staff"]);
+  const { t } = useTranslation(["pages", "staff"]);
 
-    return (
-        <Stack align={"stretch"}>
-            <Title2 text={t("about.title", {ns: "pages"})} align="center"/>
-            <SimpleGrid
-                cols={{base: 1, xs: 1, sm: 1, md: 2, lg: 3}}
-                spacing={{base: 10, sm: 'xl'}}
-                verticalSpacing={{base: 'md', sm: 'xl'}}
-            >
-                {staff.map((member) => (
-                    <StaffCard
-                        name={member.name}
-                        position={t(member.name, {ns: "staff"})}
-                        imageUrl={member.image}
-                    />
-                ))}
-            </SimpleGrid>
-            <Title2 text={t("about.socialNetworks", {ns: "pages"})} align="center"/>
-            <SocialNetworks/>
-        </Stack>
-    );
+  return (
+    <Stack align={"stretch"}>
+      <Title2 text={t("about.title", { ns: "pages" })} align="center" />
+      <SimpleGrid
+        cols={{ base: 1, xs: 1, sm: 1, md: 2, lg: 3 }}
+        spacing={{ base: 10, sm: "xl" }}
+        verticalSpacing={{ base: "md", sm: "xl" }}
+      >
+        {staff.map((member) => (
+          <StaffCard
+            name={member.name}
+            position={t(member.name, { ns: "staff" })}
+            imageUrl={member.image}
+          />
+        ))}
+      </SimpleGrid>
+      <Title2
+        text={t("about.socialNetworks", { ns: "pages" })}
+        align="center"
+      />
+      <SocialNetworks />
+    </Stack>
+  );
 }
 
-
 function SocialNetworks() {
-    const isMobile = useMediaQuery('(max-width: 48em)');
+  const isMobile = useMediaQuery("(max-width: 48em)");
 
-    return (
-        <Group justify="center" gap="xl">
-            {socialNetworks.map((social, idx) => (
-                <ActionIcon
-                    key={idx}
-                    component="a"
-                    href={social.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    variant="transparent"
-                    size={isMobile ? 50 :100}
-                    color={"white"}
-                >
-                    <social.icon size={100}/>
-                </ActionIcon>
-            ))}
-        </Group>
-    );
-
+  return (
+    <Group justify="center" gap="xl">
+      {socialNetworks.map((social, idx) => (
+        <ActionIcon
+          key={idx}
+          component="a"
+          href={social.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          variant="transparent"
+          size={isMobile ? 50 : 100}
+          color={"white"}
+        >
+          <social.icon size={80} />
+        </ActionIcon>
+      ))}
+    </Group>
+  );
 }

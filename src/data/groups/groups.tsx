782d3292b71af0d3dc2dcfa922eb03ef0b6a2115
tekofx <tekofxx@gmail.com>
2025-08-28 16:32:46 +0200

    Added furrfree steam

--- src/data/groups/groups.tsx
+++ src/data/groups/groups.tsx
@@ -1,24 +1,34 @@
-import {IconBrandDiscord, IconBrandTelegram} from "@tabler/icons-react";
+import {
+  IconBrandDiscord,
+  IconBrandTelegram,
+  IconBrandSteam,
+} from "@tabler/icons-react";
 
 const groups = [
-    {
-        id: "furrfreeSpain",
-        icon: IconBrandTelegram,
-        link: "https://t.me/FurrFreeES",
-        image: `${import.meta.env.BASE_URL}logos/ffglobal_2.svg`
-    },
-    {
-        id: "furrfreeInternational",
-        icon: IconBrandTelegram,
-        link: "https://t.me/furrfree",
-        image: `${import.meta.env.BASE_URL}logos/ffmono_whiteblack.svg`
-    },
-    {
-        id: "furrfreeDiscord",
-        icon: IconBrandDiscord,
-        link: "https://discord.gg/fZb2PFtK8B",
-        image: `${import.meta.env.BASE_URL}logos/ffglobal_3.svg`
-    },
-]
+  {
+    id: "furrfreeSpain",
+    icon: IconBrandTelegram,
+    link: "https://t.me/FurrFreeES",
+    image: `${import.meta.env.BASE_URL}logos/ffglobal_2.svg`,
+  },
+  {
+    id: "furrfreeInternational",
+    icon: IconBrandTelegram,
+    link: "https://t.me/furrfree",
+    image: `${import.meta.env.BASE_URL}logos/ffmono_whiteblack.svg`,
+  },
+  {
+    id: "furrfreeDiscord",
+    icon: IconBrandDiscord,
+    link: "https://discord.gg/fZb2PFtK8B",
+    image: `${import.meta.env.BASE_URL}logos/ffglobal_3.svg`,
+  },
+  {
+    id: "furrfreeSteam",
+    icon: IconBrandSteam,
+    link: "https://steamcommunity.com/groups/FurrFree",
+    image: `${import.meta.env.BASE_URL}logos/ffglobal_3.svg`,
+  },
+];
 
 export default groups;
